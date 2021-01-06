997a0f1957fcc08939f3bd0d3aa8684ff8c141eb
Poul Hornsleth <poulh@umich.edu>
2021-01-06 19:04:17 +0200

    Initial Commit

---
+++ Time Zone CompareTests/Time_Zone_CompareTests.swift
@@ -0,0 +1,33 @@
+//
+//  Time_Zone_CompareTests.swift
+//  Time Zone CompareTests
+//
+//  Created by Poul Hornsleth on 1/6/21.
+//
+
+import XCTest
+@testable import Time_Zone_Compare
+
+class Time_Zone_CompareTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
